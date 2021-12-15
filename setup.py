9c8b7b4e5940c6842f2c4d6c0f2336072a37f629
Juan Rueda <juan.rueda@neoris.com>
2021-12-15 10:53:40 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+# This setup is suitable for "python setup.py develop".
+
+setup(
+        name='awsneo',
+        version='0.1',
+        description='Library for python 3 that allows you to connect to different database engines (PostgreSQL, MySQL, MSSQL) through secrets manager (secret values) or direct connection, also allows you to execute queries from scripts in buckets or directly as a single query',
+        author='Juan Rueda',
+        author_email='Juan.Rueda@btgpactual.com',
+        packages=['awsneo'],
+)
